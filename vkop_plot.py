4e78aa85879b464cf3b14793906f3c98e3ed46ce
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-05 13:28:36 +0300

    Update vkop_plot.py

--- vkop_plot.py
+++ vkop_plot.py
@@ -8,11 +8,13 @@ def draw_plot(vkop, zQ, zp):
     from io import BytesIO
     Qs = [item for item in df[kluch] if type(item)==float or type(item)==int]
     list_pk = [list_p[i] for i in range(len(Qs))]
-    ip = np.array([ff(Qs, list_pk, i) for i in Qs])
+    Qq = sum([[Qs[i], (Qs[i]+Qs[i])/2] for i in range(len(Qs)-1)], [])+ [Qs[-1]]
+    Qq = np.array(Qq)
+    ip = np.array([ff(Qq, list_pk, i) for i in Qq])
     s = np.array(Qs)
     cht = np.array(list_pk)
     plt.scatter(s, cht, linewidth= 3, color="#26822F")
-    plt.plot(s, ip, linewidth=4, color="#26822F")
+    plt.plot(Qq, ip, linewidth=4, color="#26822F")
     plt.scatter(np.array(zQ), np.array(zp), color="#FF642B", linewidths=4)
     plt.grid(True)
     plt.xlabel('Расход воздуха, м³/ч')
