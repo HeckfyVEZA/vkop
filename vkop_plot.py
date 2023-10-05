4d2fa356bce6e6c27d440cb0b2fb6940d6684129
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-05 12:38:51 +0300

    Update vkop_plot.py

--- vkop_plot.py
+++ vkop_plot.py
@@ -8,7 +8,7 @@ def draw_plot(vkop, zQ, zp):
     from io import BytesIO
     Qs = [item for item in df[kluch] if type(item)==float or type(item)==int]
     list_pk = [list_p[i] for i in range(len(Qs))]
-    ip = np.array([ff(Qs, list_pk, i) for i in list_pk])
+    ip = np.array([ff(Qs, list_pk, i) for i in Qs])
     s = np.array(Qs)
     cht = np.array(list_pk)
     plt.scatter(s, ip, linewidth= 3, color="#26822F")
