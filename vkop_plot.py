496f8d647639644db9bec8d73821a1b593cf555d
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-16 17:39:51 +0300

    Update vkop_plot.py

--- vkop_plot.py
+++ vkop_plot.py
@@ -19,9 +19,10 @@ def draw_plot(vkop, zQ, zp):
     # plt.scatter(s, cht, linewidth= 3, color="#26822F")
     plt.plot(Qq, ip, linewidth=4, color="#26822F")
     plt.scatter(np.array(zQ), np.array(zp), color="#FF642B", linewidths=4)
-    kpd_p = [f(zQ, zp, i) for i in Qs]
+    kpd_p = [f(zQ, zp, i) for i in Qs if f(zQ, zp, i)<=900]
+    Qsn = Qs[:len(kpd_p)]
     dot = kpd_find(Qs, list_pk, zQ, zp)
-    plt.plot(Qs, kpd_p, linewidth=3, color="#48B454")
+    plt.plot(Qsn, kpd_p, linewidth=3, color="#48B454")
     plt.scatter([dot[0]], [dot[1]], linewidth=4, color="#48B454")
     plt.grid(True)
     plt.xlabel('Расход воздуха, м³/ч')
