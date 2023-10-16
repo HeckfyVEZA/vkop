67d08e187836267a935839227f25f08e43481a24
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-16 17:16:34 +0300

    Update interpolate.py

--- interpolate.py
+++ interpolate.py
@@ -13,3 +13,22 @@ def forming_formula(lx, ly, x):
     t = np.poly1d(np.polyfit(xi, yi, 5))
     a = t(x)
     return a
+def f(x1, y1, x):
+    return (y1/(x1**2))*x**2
+def anti_f(x1, y1, y):
+    return (((x1**2)/y1)*y)**.5
+def kpd_find(rx, ry, xi, yi):
+    t = np.poly1d(np.polyfit(rx, ry, 5))
+    cx = xi
+    cy = f(xi, yi, cx)
+    ny = t(cx)
+    eps = 10
+    while eps>0.1:
+        cy = f(xi, yi, cx)
+        ny = t(cx)
+        my = cy + ((ny - cy) / 2)
+        cx = anti_f(xi, yi, my)
+        eps = ((f(xi, yi, cx) - t(cx))**2)**.5
+    cx = int(round(cx, 0))
+    cy = f(xi, yi, cx)
+    return [cx, cy]
