6c2c74f73c21fa13df83cb0de9df66b108e3f370
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-16 14:58:32 +0300

    Update vkop.py

--- vkop.py
+++ vkop.py
@@ -112,7 +112,8 @@ try:
         import json
         from io import BytesIO
         output = BytesIO(json.dump(infoss))
-        # json.dump(infoss, output)
+        with open(output, "w") as fp: 
+            json.dump(infoss, fp)
         return output
     st.download_button('Скачать лист подбора', data=doc_fil([f"{st.session_state.innumber} от {st.session_state.fromnum}",st.session_state.orderer,st.session_state.object,st.session_state.system,st.session_state.manager,st.session_state.engineer,f"{st.session_state.VKOP}-{item[0]}-{st.session_state.climate}",st.session_state.Q,st.session_state.p,item[1],item[2],image_vkop[st.session_state.VKOP],st.session_state.plot,gdf,dops], filial=st.session_state.filial), file_name=f"{st.session_state.system}.docx")
     st.download_button("Скачать JSON", data=json_fo({"project_num":st.session_state.innumber, "project_date":st.session_state.fromnum,
