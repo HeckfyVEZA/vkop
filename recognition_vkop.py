4815d7a7379f4e5a0ee7dc461bd143a6fed30a6c
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-17 11:07:56 +0300

    Update recognition_vkop.py

--- recognition_vkop.py
+++ recognition_vkop.py
@@ -3,4 +3,5 @@ from interpolate import forming_formula as ff
 corQ = lambda x: [i for i in x if i!=None]
 def vkop_list(p, Q):
     variants = [[key, Q, ff(df[key], list_p, Q)] for key in df.keys() if -50<=(ff(df[key], list_p, Q) - p)<=200 and 75<=ff(df[key], list_p, Q)<=950 and min(corQ(df[key]))<=Q<=max(corQ(df[key]))]
+    variants = sorted(variants, key=lambda x: ((p - x[2])**2)**.5)
     return variants
