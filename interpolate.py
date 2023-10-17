0dd1de20e46178c4cf4e5e8f5ff4c823246e26d8
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-17 10:45:58 +0300

    Update interpolate.py

--- interpolate.py
+++ interpolate.py
@@ -1,8 +1,6 @@
 def forming_formula(lx, ly, x):
     lx = [i for i in lx if i!=None]
     ly = ly[:len(lx)]
-    lx = sum([[lx[i], (lx[i]+lx[i])/2] for i in range(len(lx)-1)], [])+ [lx[-1]]
-    ly = sum([[ly[i], (ly[i]+ly[i])/2] for i in range(len(ly)-1)], [])+ [ly[-1]]
     import numpy as np
     xi = np.array(lx)
     yi = np.array(ly)
