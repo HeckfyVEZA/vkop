cf5aa1bba4bd31b9952056b6f149281bad462d2f
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-23 11:14:24 +0300

    Update interpolate.py

--- interpolate.py
+++ interpolate.py
@@ -14,6 +14,12 @@ def forming_formula(lx, ly, x):
     return a
 def f(x1, y1, x):
     return (y1/(x1**2))*x**2
+
+def lin_f(x1, y1, x):
+    return (y1/(x1))*x
+
+def anti_lin_f(x1, y1, y):
+    return ((y1/(x1))**(-1))*y
 def anti_f(x1, y1, y):
     return (((x1**2)/y1)*y)**.5
 def kpd_find(lx, ly, xi, yi):
@@ -28,18 +34,14 @@ def kpd_find(lx, ly, xi, yi):
     while eps>1:
         ii+=1
         print(cx)
-        cy = f(xi, yi, cx)
+        if ii > 10:
+            cy = lin_f(xi, yi, cx)
+            # cx = anti_lin_f(xi, yi, my)
+        else:
+            cy = f(xi, yi, cx)
         ny = t(cx)
         my = cy + ((ny - cy) / 2)
         cx = anti_f(xi, yi, my)
-        if cx > max(lx):
-            cx = max(lx)
-        if cx < min(lx):
-            cx = min(lx)
-        xes.append(cx)
-        if ii>5:
-            cx = pr(xes)**(1/len(xes))
-            ii = 0
         
         eps = ((f(xi, yi, cx) - t(cx))**2)**.5
     # print()
