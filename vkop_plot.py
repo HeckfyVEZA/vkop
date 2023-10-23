6289fa14635a4d5bb9800be9c5e96f0c40754001
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-23 14:24:06 +0300

    Update vkop_plot.py

--- vkop_plot.py
+++ vkop_plot.py
@@ -24,10 +24,14 @@ def draw_plot(vkop, zQ, zp):
     dot = kpd_find(Qs, list_pk, zQ, zp)
     # kpd_p = [k for k in kpd_p if dot[1]-150<=k<=dot[1]+150 and dot[0]-5000<=anti_f(zQ, zp, k)<=dot[0]+5000]
     # Qsn = [anti_f(zQ, zp, i) for i in kpd_p]
-    Qsn = [zQ, dot[0]]
-    kpd_p = [zp, dot[1]]
+    Qsn = [-dot[0], -zQ, 0, zQ, dot[0]]
+    kpd_p = [dot[1],zp,0, zp, dot[1]]
+    pol_kpd = np.poly1d(np.ployfit(Qsn, kpd_p, 2))
+    ab_d  = ((zQ-dot[0])**2)**.5
+    pQsn = [zQ-ab_d, zQ, dot[0], dot[0] + ab_d]
+    pkpd_p = [pol_kpd(qwe) for qwe in p_Qsn]
     # dot = kpd_find(Qs, list_pk, zQ, zp)
-    plt.plot(np.array(Qsn), np.array(kpd_p), linewidth=1, color="#48B454")
+    plt.plot(np.array(pQsn), np.array(pkpd_p), linewidth=1, color="#48B454")
     plt.scatter([dot[0]], [dot[1]], linewidth=4, color="#48B454")
     plt.scatter(np.array(zQ), np.array(zp), color="#FF642B", linewidths=4)
     plt.grid(True)
