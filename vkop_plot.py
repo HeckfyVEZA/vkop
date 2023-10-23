a4bfe4401e23930246137b53aee9df63c123b446
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-10-23 13:04:28 +0300

    Update vkop_plot.py

--- vkop_plot.py
+++ vkop_plot.py
@@ -29,7 +29,7 @@ def draw_plot(vkop, zQ, zp):
     plt.grid(True)
     plt.xlabel('Расход воздуха, м³/ч')
     plt.ylabel('Статическое давление, Па')
-    plt.ylim(50, max(list_pk)+50)
+    plt.ylim(50, max(list_pk)+5)
     memfile = BytesIO()
     plt.savefig(memfile)
     return memfile
