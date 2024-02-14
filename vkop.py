0e1e0a56c8c389e3b2b323cc16e338c0563a8629
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2024-02-14 14:21:41 +0300

    Update vkop.py

--- vkop.py
+++ vkop.py
@@ -96,13 +96,16 @@ try:
     else:
         dops = []
     from temp_fill import doc_fil
-    st.download_button('Скачать лист подбора', data=doc_fil([f"{st.session_state.innumber} от {st.session_state.fromnum}",st.session_state.orderer,st.session_state.object,st.session_state.system,st.session_state.manager,st.session_state.engineer,f"{st.session_state.VKOP}-{item[0]}-{st.session_state.climate}",st.session_state.Q,st.session_state.p,item[1],item[2],image_vkop[st.session_state.VKOP],st.session_state.plot,gdf,dops], filial=st.session_state.filial), file_name=f"{st.session_state.system}.docx")
-    st.download_button("Скачать JSON", data=json_fo({"project_num":st.session_state.innumber, "project_date":st.session_state.fromnum,
+    try:
+        st.download_button('Скачать лист подбора', data=doc_fil([f"{st.session_state.innumber} от {st.session_state.fromnum}",st.session_state.orderer,st.session_state.object,st.session_state.system,st.session_state.manager,st.session_state.engineer,f"{st.session_state.VKOP}-{item[0]}-{st.session_state.climate}",st.session_state.Q,st.session_state.p,item[1],item[2],image_vkop[st.session_state.VKOP],st.session_state.plot,gdf,dops], filial=st.session_state.filial), file_name=f"{st.session_state.system}.docx")
+        st.download_button("Скачать JSON", data=json_fo({"project_num":st.session_state.innumber, "project_date":st.session_state.fromnum,
  "client": st.session_state.orderer, "object_name": st.session_state.object,
  "system_number":st.session_state.system, "manager":st.session_state.manager, 
  "engineer":st.session_state.engineer, "fan_name": f"{st.session_state.VKOP}-{item[0]}-{st.session_state.climate}", 
  "given_Q":st.session_state.Q, "given_p":st.session_state.p, "real_Q":item[1], "real_p":item[2], "extra":dops
 }), file_name=f"{st.session_state.system}.json")
+    except Exception as AAA:
+        st.write(AAA)
 except Exception as er:
     pass
     st.toast(f":red[{er}]")
